35c2b4a0449af9fa31c7d789db819349160a70a0
Zia Uddin Ahmed <43880773+alligator609@users.noreply.github.com>
2019-05-03 10:25:36 +0600

    Update ArtistList.java

--- app/src/main/java/com/zia/alligator/firebasecrudrndnew/ArtistList.java
+++ app/src/main/java/com/zia/alligator/firebasecrudrndnew/ArtistList.java
@@ -8,9 +8,7 @@ import android.widget.TextView;
 
 import java.util.List;
 
-/**
- * Created by Showrav on 11/3/2017.
- */
+
 
 public class ArtistList extends ArrayAdapter<ArtistClass> {
     private Activity context;
